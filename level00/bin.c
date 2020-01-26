e48985665a5b393777d5097e6406f7fed46e2991
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-01-26 16:59:14 +0100

    add level0

---
+++ level00/bin.c
@@ -0,0 +1,20 @@
+int	main()
+{
+	int	pass;
+
+	puts("***********************************");
+	puts("* \t     -Level00 -\t\t  *");
+	puts("***********************************");
+
+	printf("Password:");
+	scanf("%d", &pass);
+
+	if (pass == 5276) {
+		puts("\nAuthenticated!");
+		system("/bin/sh");
+		return 0;
+	}
+
+	puts("\nInvalid Password!");
+	return 0;
+}
---
+++ level00/walkthrough.md
@@ -0,0 +1,11 @@
+The binary is pretty explicit: we just need to write 5276 and we get a shell.
+```shell
+bash$ ./level00
+***********************************
+* 	     -Level00 -		  *
+***********************************
+Password:5276
+Authenticated!
+$ cat /home/users/level01/.pass
+uSq2ehEGT6c9S24zbshexZQBXUGrncxn5sD5QfGL
+```
