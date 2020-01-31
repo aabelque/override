c0f522adfa3d2c254dce0bcbf4954fd4365cd762
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-01-31 16:47:54 +0100

    better level04 binary

--- level04/bin.c
+++ level04/bin.c
@@ -1,41 +1,37 @@
 int	main()
 {
-	int	x;
+	int	stat_loc;
 	char	buf[128];
 	int	z;
 	int	w;
-	int	y;
+	long	ptrace_ret;
 	pid_t	child;
 
 	child = fork();
 	x = 0;
-	y = 0;
+	ptrace_ret = 0;
 	memset(buf, 0, 32);
 
 	if (child == 0) {
 		prctl(1, 1);
-		ptrace(0, 0, 0, 0);
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
 		puts("Give me some shellcode, k");
 		gets(buf);
 		return 0;
 	}
 
-	while (y != 11) {
-		wait(x);
+	while (ptrace_ret != 11) {
+		wait(&x);
 
-		z = x;
-		z &= 0x7f;
-		w = x;
-		w &= 0x7f;
-		w += 1;
-		w = w >> 1;
+		z = x & 0x7f;
+		w = ((x & 0x7f) + 1) >> 1;
 
 		if (z == 0 || w > 0) {
 			puts("child is exiting...");
 			return 0;
 		}
 
-		y = ptrace(3, child, 44, 0);
+		ptrace_ret = ptrace(PTRACE_PEEKUSER, child, 44, 0);
 	}
 
 	puts("no exec() for you");
